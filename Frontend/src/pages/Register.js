a821f1e75b83d29422701b8d5337ea3682a2b6d4
Rwan Khalifa <rwankhalifa@Rwans-MacBook-Air.local>
2024-05-21 04:09:12 +0300

    react-setup

---
+++ Frontend/src/pages/Register.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div>
+      <h2>Register</h2>
+      <form>
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
